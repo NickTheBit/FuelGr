f3e8bc5965b269ebdd97962e716d50665cf76c15
NickTheBit <nikosgklu@gmail.com>
2021-01-23 16:09:23 +0200

    configured settings for bootstrap

--- fuelGr/settings.py
+++ fuelGr/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = (
+    '/website/static/',
+)
+
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
