381b342e5a982242d9f662ad59ce61df414aae48
NickTheBit <nikosgklu@gmail.com>
2021-01-23 15:32:06 +0200

    Removed faulty test

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -4,12 +4,9 @@ from .models import Users
 
 # Create your tests here.
 
-class UserCreation(TestCase):
-    def setUp(self):
-        users = User.objects.all()
-        for i in users:
-            user = User.objects.create_user(username=i.username,email=i.email,password=i.password)
-
-    def test(self):
-        print("ok")
-            
+# This is a test example
+# class UserCreation(TestCase):
+#     def setUp(self):
+#         users = User.objects.all()
+#         for i in users:
+#             user = User.objects.create_user(username=i.username,email=i.email,password=i.password)
